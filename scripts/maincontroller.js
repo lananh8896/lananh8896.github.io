4e85fe628e6e55a7fcba2888dba482689d34d42a
lananh8896 <nguyenthilananh8896@gmail.com>
2017-09-09 15:59:18 +0700

    update

--- scripts/maincontroller.js
+++ scripts/maincontroller.js
@@ -1,10 +1,10 @@
-var scotchApp = angular.module('scotchApp', ['ngRoute', 'ngTagsInput', 'textAngular', 'ui.bootstrap']);
+var scotchApp = angular.module('scotchApp', ['ngRoute', 'ngTagsInput', 'textAngular', 'ui.bootstrap', 'ngCookies']);
 scotchApp.config(['$locationProvider', function($locationProvider) {
     $locationProvider.hashPrefix('');
 }]);
 
 
-scotchApp.controller('maincontroller', function($scope, $http, $routeParams, ) {
+scotchApp.controller('maincontroller', function($scope, $http, $routeParams, $cookieStore) {
 
     var root = "https://green-web-blog.herokuapp.com";
 
@@ -60,6 +60,8 @@ scotchApp.controller('maincontroller', function($scope, $http, $routeParams, ) {
     };
 
 
+
+
     $scope.getArticle = function() {
         $scope.currentArticleId = $routeParams.id;
         $http.get(root + "/api/articles")
@@ -111,42 +113,57 @@ scotchApp.controller('maincontroller', function($scope, $http, $routeParams, ) {
         }
     }
 
-    $scope.login = function() {
+    // $scope.login = function() {
 
-        console.log($scope.user);
+    //     console.log($scope.user);
 
-        //POST Login API below:
-        $http.post(root + '/api/users/auth', $scope.user)
-            .success(function(response) {
+    //     //POST Login API below:
+    //     $http.post(root + '/api/users/auth', $scope.user)
+    //         .success(function(response) {
 
-                var isSuccess = response.success;
-                if (isSuccess) {
-                    console.log(response);
-                } else {
-                    //Raise Error
-                    alert(response.message);
-                }
+    //             var isSuccess = response.success;
+    //             if (isSuccess) {
+    //                 console.log(response);
+    //             } else {
+    //                 //Raise Error
+    //                 alert(response.message);
+    //             }
 
-            }).error(function(data, status, headers, config) {
+    //         }).error(function(data, status, headers, config) {
+    //             console.log(data, status, headers, config);
+    //         });
+    // };
+
+    // $scope.signup = function() {
+    //     //POST Login API below:
+    //     $http.post(root + '/api/users/auth', $scope.newUser)
+    //         .success(function(response) {
+    //             var isSuccess = response.success;
+    //             if (isSuccess) {
+    //                 console.log(response);
+    //             } else {
+    //                 //Raise Error
+    //                 alert(response.message);
+    //             }
+    //         }).error(function(data, status, headers, config) {
+    //             console.log(data, status, headers, config);
+    //         });
+    // };
+
+    // Comment coppy 
+    $scope.addCommentforArticle = function() {
+        $scope.newComment._user = myId;
+        $http.put(root + '/api/article/comment/' + $scope.article._id, $scope.newComment)
+            .then(function successCallbak(response) {
+                $scope.article = response.data;
+                alert("Thành công");
+                console.log(response.data);
+            }, function errorCallback(response) {
                 console.log(data, status, headers, config);
             });
-    };
+    }
 
-    $scope.signup = function() {
-        //POST Login API below:
-        $http.post(root + '/api/users/auth', $scope.newUser)
-            .success(function(response) {
-                var isSuccess = response.success;
-                if (isSuccess) {
-                    console.log(response);
-                } else {
-                    //Raise Error
-                    alert(response.message);
-                }
-            }).error(function(data, status, headers, config) {
-                console.log(data, status, headers, config);
-            });
-    };
+
 
 
     //AllArticleinCategories tam
@@ -224,6 +241,69 @@ scotchApp.controller('maincontroller', function($scope, $http, $routeParams, ) {
             }
         };
     });
+
+    $scope.signup = function() {
+        $http.post(root + '/api/users/signup/', $scope.newUser).then(function successCallbak(response) {
+            var isSuccess = response.data.success;
+            if (isSuccess === true) {
+                $cookieStore.put('token', response.data.token);
+                $cookieStore.put('user', response.data.user);
+                $scope.user = $cookieStore.get('user');
+                $scope.token = $cookieStore.get('token');
+                //Redirect here
+                window.location.href = '/#/'
+            } else {
+                //Raise Error
+                alert(response.message);
+            }
+        }, function errorCallback(response) {
+            console.log(data, status, headers, config);
+        });
+    };
+    $scope.login = function() {
+        console.log($scope.user);
+
+        $http.post(root + '/api/users/auth', $scope.user)
+            .then(function successCallback(response) {
+                var isSuccess = response.data.success;
+                if (isSuccess === true) {
+                    $cookieStore.put('token', response.data.token);
+                    $cookieStore.put('user', response.data.user);
+                    $scope.user = $cookieStore.get('user');
+                    $scope.token = $cookieStore.get('token');
+                    //Redirect here
+                    window.location.href = '/#/'
+                } else {
+                    //Raise Error
+                    // alert(response.message);
+                }
+            }, function errorCallback(response) {
+                console.log(data, status, headers, config);
+            });
+    };
+
+    $scope.loadLogin = function() {
+        var token = $cookieStore.get('token');
+        if (token !== undefined) {
+            $location.url("/")
+        }
+    }
+
+    $scope.isLogged = function() {
+        if ($cookieStore.get('token') != undefined) {
+
+            return true;
+
+        } else {
+            return false;
+        }
+    }
+    $scope.logOut = function() {
+        $cookieStore.remove('token');
+        $cookieStore.remove('user');
+    }
+
+
 
 
 });
